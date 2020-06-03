ec55eb199b6f452e3bc9330fea6cd509edd548a1
ironova <trachel.romain@ironova.com>
2020-06-03 18:59:07 -0400

    adding style controls

--- runway_model.py
+++ runway_model.py
@@ -5,11 +5,14 @@ import dnnlib.tflib as tflib
 import runway
 
 fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
+n_styles = 16
 
 @runway.setup(options={'checkpoint': runway.file(extension='.pkl')})
 def setup(opts):
     global Gs
     tflib.init_tf()
+    if opts['checkpoint'] is None:
+        opts['checkpoint'] = 'checkpoints\\network-snapshot-000600.pkl'
     with open(opts['checkpoint'], 'rb') as file:
         _G, _D, Gs = pickle.load(file, encoding='latin1')
     noise_vars = [var for name, var in Gs.components.synthesis.vars.items() if name.startswith('noise')]
@@ -19,16 +22,32 @@ def setup(opts):
 
 
 generate_inputs = {
-    'z': runway.vector(512, sampling_std=0.5),
+    'z': runway.vector(256, sampling_std=0.5),
     'truncation': runway.number(min=0, max=1, default=0.8, step=0.01)
 }
+
+for i in range(4):
+    for j in range(4):
+        generate_inputs.update({'style_%i/%i' %(i, j): runway.number(min=0, max=1, default=0.5, step=0.01)})
 
 @runway.command('generate', inputs=generate_inputs, outputs={'image': runway.image})
 def convert(model, inputs):
     z = inputs['z']
     truncation = inputs['truncation']
-    latents = z.reshape((1, 512))
-    images = model.run(latents, None, truncation_psi=truncation, randomize_noise=False, output_transform=fmt)
+    latent = z.reshape((1, 256))
+    style_mix = np.zeros([4, 4])
+    for i in range(4):
+        for j in range(4):
+            style_mix[i, j] = inputs['style_%i/%i' %(i, j)]
+
+    style_mix = style_mix.reshape(-1)
+    dlatent = model.components.mapping.run(latent, None)
+    mixed_dlatent = dlatent.copy()
+    for i in range(n_styles):
+        mixed_dlatent[0][i] *= style_mix[i]
+
+    images = model.components.synthesis.run(mixed_dlatent, truncation_psi=truncation, randomize_noise=False, output_transform=fmt)
+    #images = model.run(latents, None, truncation_psi=truncation, randomize_noise=False, output_transform=fmt)
     output = np.clip(images[0], 0, 255).astype(np.uint8)
     return {'image': output}
 
